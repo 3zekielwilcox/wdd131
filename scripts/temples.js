b659aeb7684529521f96bd60a5f2116d91045938
3zekielwilcox <ezekielwicox@icloud.com>
2024-05-09 12:47:42 -0600

    temples fixed

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,18 +1,26 @@
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+  navigation.classList.toggle('open');
+  hamButton.classList.toggle('open');
+});
+
 document.addEventListener('DOMContentLoaded', function() {
-    // Get the current year
-    const currentYear = new Date().getFullYear();
-  
-    // Find the span element with the ID "currentyear"
-    const yearSpan = document.getElementById('currentyear');
-  
-    // Update the content of the span element with the current year
-    yearSpan.textContent = currentYear;
+  // Get the current year
+  const currentYear = new Date().getFullYear();
+
+  // Find the span element with the ID "currentyear"
+  const yearSpan = document.getElementById('currentyear');
+
+  // Update the content of the span element with the current year
+  yearSpan.textContent = currentYear;
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Last modified information
+    const lastModified = document.getElementById("lastModified");
+    const lastModifiedDate = document.lastModified;
+    lastModified.textContent = `Last Modified: ${lastModifiedDate}`;
   });
   
-  document.addEventListener("DOMContentLoaded", function () {
-      // Last modified information
-      const lastModified = document.getElementById("lastModified");
-      const lastModifiedDate = document.lastModified;
-      lastModified.textContent = `Last Modified: ${lastModifiedDate}`;
-    });
-    
