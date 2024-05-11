9232260ee5497c8a46f1eb31eb427b12f8814a4f
3zekielwilcox <ezekielwicox@icloud.com>
2024-05-11 14:07:52 -0600

    place

---
+++ scripts/place.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Get the current year
+    const currentYear = new Date().getFullYear();
+  
+    // Find the span element with the ID "currentyear"
+    const yearSpan = document.getElementById('currentyear');
+  
+    // Update the content of the span element with the current year
+    yearSpan.textContent = currentYear;
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Last modified information
+    const lastModified = document.getElementById("lastModified");
+    const lastModifiedDate = document.lastModified;
+    lastModified.textContent = `Last Modified: ${lastModifiedDate}`;
+});
+
+function fetchWeather() {
+
+    const temperature = 25;
+    const condition = "Sunny";
+    const windSpeed = 10;
+    const windchill= 'N/A';
+
+    // Update the DOM with weather information
+    document.getElementById('temperature').textContent = temperature + " °C";
+    document.getElementById('condition').textContent = condition;
+    document.getElementById('wind-speed').textContent = windSpeed + " m/s";
+    document.getElementById('wind-chill').textContent = windchill;
+}
+
+window.addEventListener('load', fetchWeather);
